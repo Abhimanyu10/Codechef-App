2b4b404a3600b49b5c79012710ee0e608d028d27
Abhimanyu <abhimanyu19982002@gmail.com>
2020-03-23 16:12:02 +0530

    Initial Commit

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import 
+export default class Home extends Component {
+    render() {
+        return (
+            <>
+                <div>
+                    <div><ImageSlider/></div>
+                    <div>Image Slider Component Rendered here</div>
+                </div>
+                <div>
+                    <LoginPage/>
+                </div>
+            </>
+        )
+    }
+}
