b21f35f04cc61d0c91fc3acaacb255f84fe41dc7
Abhimanyu <abhimanyu19982002@gmail.com>
2020-03-23 18:06:58 +0530

    Created outline for recent activity component and added timer component

--- src/pages/contest/recent_activity/RecentActivity.jsx
+++ src/pages/contest/recent_activity/RecentActivity.jsx
@@ -1,6 +1,25 @@
 import React, { Component } from 'react'
 
 export default class RecentActivity extends Component {
+
+
+    constructor(props) {
+        super(props);
+        this.fetch_recent_activity = this.fetch_recent_activity.bind(this)
+        this.state = {
+            //have to decide state yet
+        };
+    }
+
+
+    fetch_recent_activity() {
+        //fetching the recent activity through php
+    }
+
+    componentDidMount() {
+        // fetch_recent_activity()        
+    }
+
     render() {
         return (
             <div>
